6c1a5a9008abfcce2166c6957c4fc7c9eec5cf59
Sky Brewer <sky.brewer@ess.eu>
2024-12-09 15:09:15 +0100

    swap to 32

--- archiver_ioc.py
+++ archiver_ioc.py
@@ -61,13 +61,13 @@ async def runner():
     print("start")
     await start_server_pv(
         create_pvs("ARCH:PERIOD:14Hz:MONITOR",  [
-        WritingOptions(20 + 60 * 14, 20.0, 1.0, 20.0),
+        WritingOptions(20 + 60 * 14, 20.0, 1.0, 32.0),
     ])
-        | create_pvs("ARCH:PERIOD:20Hz:MONITOR",  [
-        WritingOptions(60 * 14, 0.0, 1.0, 20.0),
+        | create_pvs("ARCH:PERIOD:32Hz:MONITOR",  [
+        WritingOptions(60 * 14, 0.0, 1.0, 32.0),
     ])
         | create_pvs("ARCH:PERIOD:14Hz:SCAN",  [
-        WritingOptions(40 + 60 * 14, 40.0, 1.0, 20.0),
+        WritingOptions(40 + 60 * 14, 40.0, 1.0, 32.0),
     ])
     )
     print("end")
